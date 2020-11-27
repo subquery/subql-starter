6318b02859d4c8ef809d9ee3212c0787d42ad8a8
Ian He <ian@onfinality.io>
2020-11-27 14:10:25 +1300

    remove api from handleBlock fn

--- extrinsics/src/mappings/Extrinsic.ts
+++ extrinsics/src/mappings/Extrinsic.ts
@@ -1,16 +1,12 @@
 
 import {Extrinsic} from "../types/extrinsic/Extrinsic";
 import {SignedBlock} from "@polkadot/types/interfaces";
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const provider = new WsProvider("wss://rpc.polkadot.io");
 
 export async function handleBlock(thisBlock: SignedBlock): Promise<void> {
-
-    const api = await ApiPromise.create({ provider });
     const blockNum = thisBlock.block.header.number.toString();
-    const blockHash = await api.rpc.chain.getBlockHash(blockNum);
+    const blockHash = thisBlock.block.header.hash.toString();
 
-    let entity = new Extrinsic(blockHash.toJSON());
+    let entity = new Extrinsic(blockHash);
     entity.id= blockNum;
     entity.NumExtrinsic = thisBlock.block.extrinsics.length;
     await entity.save();
