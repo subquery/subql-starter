c53d8b781fe3e5cc5864b91305fd17ef60f5577d
James Bayly <bayly.james@gmail.com>
2022-09-24 17:32:07 +1200

    Update Moonbeam projects

--- Astar/astar-evm-starter/src/mappings/mappingHandlers.ts
+++ Astar/astar-evm-starter/src/mappings/mappingHandlers.ts
@@ -1,43 +1,40 @@
 import { Approval, Transaction } from "../types";
-import {
-  FrontierEvmEvent,
-  FrontierEvmCall,
-} from "@subql/frontier-evm-processor";
-import { BigNumber } from "ethers";
+import { WasmCall, WasmEvent } from "@subql/substrate-wasm-processor";
+import { Balance, AccountId } from "@polkadot/types/interfaces/runtime";
+import { Option } from "@polkadot/types-codec";
 
 // Setup types from ABI
-type TransferEventArgs = [string, string, BigNumber] & {
-  from: string;
-  to: string;
-  value: BigNumber;
-};
-type ApproveCallArgs = [string, BigNumber] & {
-  _spender: string;
-  _value: BigNumber;
-};
+type ApproveCallArgs = [AccountId, Balance];
+type TransferEventArgs = [Option<AccountId>, Option<AccountId>, Balance];
 
-export async function handleFrontierEvmEvent(
-  event: FrontierEvmEvent<TransferEventArgs>
+export async function handleWasmCall(
+  call: WasmCall<ApproveCallArgs>
 ): Promise<void> {
-  const transaction = new Transaction(event.transactionHash);
-
-  transaction.value = event.args.value.toBigInt();
-  transaction.from = event.args.from;
-  transaction.to = event.args.to;
-  transaction.contractAddress = event.address;
-
-  await transaction.save();
+  const approval = new Approval(`${call.blockNumber}-${call.idx}`);
+  approval.hash = call.hash;
+  approval.owner = call.from.toString();
+  if (typeof call.data !== "string") {
+    const [spender, value] = call.data.args;
+    approval.spender = spender.toString();
+    approval.value = value.toBigInt();
+  } else {
+    logger.info(`Decode call failed ${call.hash}`);
+  }
+  approval.contractAddress = call.dest.toString();
+  await approval.save();
 }
 
-export async function handleFrontierEvmCall(
-  event: FrontierEvmCall<ApproveCallArgs>
+export async function handleSubstrateWasmEvent(
+  event: WasmEvent<TransferEventArgs>
 ): Promise<void> {
-  const approval = new Approval(event.hash);
-
-  approval.owner = event.from;
-  approval.value = event.args._value.toBigInt();
-  approval.spender = event.args._spender;
-  approval.contractAddress = event.to;
-
-  await approval.save();
+  const [from, to, value] = event.args;
+  const transaction = new Transaction(
+    `${event.blockNumber}-${event.eventIndex}`
+  );
+  transaction.transactionHash = event.transactionHash;
+  transaction.value = value.toBigInt();
+  transaction.from = from.toString();
+  transaction.to = to.toString();
+  transaction.contractAddress = event.contract.toString();
+  await transaction.save();
 }
