fc51e726c970b8bc6c889176d887cdc4688d3590
MOZGIII <mike-n@narod.ru>
2022-11-03 02:20:18 +0400

    Proper Humanode-specific example

--- Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
+++ Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
@@ -3,47 +3,33 @@ import {
   SubstrateEvent,
   SubstrateBlock,
 } from "@subql/types";
-import { BioAuth, StarterEntity } from "../types";
-import { Balance } from "@polkadot/types/interfaces";
+import { BioauthNewAuthentication } from "../types";
 
-const BIOAUTH_EXT_METHOD = Uint8Array.from(Buffer.from("0x0400", "hex"));
+export async function handleBlock(block: SubstrateBlock): Promise<void> {}
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  //Create a new starterEntity with ID using block hash
-  let record = new StarterEntity(block.block.header.hash.toString());
-  //Record block number
-  record.field1 = block.block.header.number.toNumber();
-  await record.save();
-}
+export async function handleEvent(
+  substrateEvent: SubstrateEvent
+): Promise<void> {
+  const { event, block, idx } = substrateEvent;
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  const {
-    event: {
-      data: [account, balance],
-    },
-  } = event;
-  //Retrieve the record by its ID
-  const record = await StarterEntity.get(
-    event.block.block.header.hash.toString()
-  );
-  record.field2 = account.toString();
-  //Big integer type Balance of a transfer event
-  record.field3 = (balance as Balance).toBigInt();
-  await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-  if (extrinsic.extrinsic.method.callIndex === BIOAUTH_EXT_METHOD) {
-    const record = new BioAuth(extrinsic.block.block.header.hash.toString());
-    //Record block number
-    record.blockNumber = extrinsic.block.block.header.number.toNumber();
-    record.signer = extrinsic.extrinsic.signer.toString();
-    record.signature = extrinsic.extrinsic.signature.toString();
-
-    record.ticket = extrinsic.extrinsic.method.args["req"]["ticket"];
-    record.ticketSignature =
-      extrinsic.extrinsic.method.args["req"]["ticketSignature"];
-
-    await record.save();
+  if (event.section != "bioauth" || event.method != "NewAuthentication") {
+    // Not what we are looking for, skip this event.
+    // If needed, we could route the event handling logic based on these fields.
+    return;
   }
+
+  const {
+    data: [validatorPublicKey],
+  } = event;
+
+  // Retrieve the record by its ID.
+  const record = new BioauthNewAuthentication(
+    `${block.block.header.number}-${idx}`
+  );
+  record.blockNumber = block.block.header.number.toNumber();
+  record.validatorPublicKey = validatorPublicKey.toString();
+  record.timestamp = block.timestamp;
+  await record.save();
 }
+
+export async function handleCall(call: SubstrateExtrinsic): Promise<void> {}
