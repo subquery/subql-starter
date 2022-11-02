c5278ace573d4b923fd1e9058c1f12f6f257d877
MOZGIII <mike-n@narod.ru>
2022-11-03 03:35:52 +0400

    Another example

--- Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
+++ Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
@@ -1,35 +1,45 @@
-import {
-  SubstrateExtrinsic,
-  SubstrateEvent,
-  SubstrateBlock,
-} from "@subql/types";
-import { BioauthNewAuthentication } from "../types";
+import { Codec } from "@polkadot/types/types";
+import { Vec } from "@polkadot/types-codec";
+import { SubstrateEvent } from "@subql/types";
+import { BioauthNewAuthentication, ImOnlineSomeOffline } from "../types";
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {}
-
-export async function handleEvent(
+export async function handleBioauthNewAuthenticationEvent(
   substrateEvent: SubstrateEvent
 ): Promise<void> {
   const { event, block, idx } = substrateEvent;
-
-  if (event.section != "bioauth" || event.method != "NewAuthentication") {
-    // Not what we are looking for, skip this event.
-    // If needed, we could route the event handling logic based on these fields.
-    return;
-  }
 
   const {
     data: [validatorPublicKey],
   } = event;
 
-  // Retrieve the record by its ID.
   const record = new BioauthNewAuthentication(
     `${block.block.header.number}-${idx}`
   );
   record.blockNumber = block.block.header.number.toNumber();
-  record.validatorPublicKey = validatorPublicKey.toString();
   record.timestamp = block.timestamp;
+  record.validatorPublicKey = validatorPublicKey.toString();
   await record.save();
 }
 
-export async function handleCall(call: SubstrateExtrinsic): Promise<void> {}
+export async function handleImonlineSomeOfflineEvent(
+  substrateEvent: SubstrateEvent<[]>
+): Promise<void> {
+  const { event, block, idx } = substrateEvent;
+
+  const {
+    data: [offline],
+  } = event;
+
+  const record = new ImOnlineSomeOffline(`${block.block.header.number}-${idx}`);
+  record.blockNumber = block.block.header.number.toNumber();
+  record.timestamp = block.timestamp;
+  record.accountIds = [];
+  for (const identification of offline as Vec<Codec>) {
+    const [accountId, _fullIdentification] = identification as any as [
+      Codec,
+      Codec
+    ];
+    record.accountIds.push(accountId.toString());
+  }
+  await record.save();
+}
