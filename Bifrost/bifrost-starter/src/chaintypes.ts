2c5a3e00e01e9b33e2cf3fbc910f857e7c09c164
James Bayly <bayly.james@gmail.com>
2022-11-21 16:39:40 +1300

    Add Chaintypes Overrides

--- Bifrost/bifrost-starter/src/chaintypes.ts
+++ Bifrost/bifrost-starter/src/chaintypes.ts
@@ -1,3 +1,8 @@
 import { typesBundleForPolkadot } from "@bifrost-finance/type-definitions";
 
-export default { typesBundle: typesBundleForPolkadot };
+export default {
+  typesBundle: typesBundleForPolkadot,
+  types: {
+    DispatchError: "DispatchErrorPre6First",
+  },
+};
