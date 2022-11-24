9786a95f4077aceb5e50e52f8344a981e8d60105
MOZGIII <mike-n@narod.ru>
2022-11-24 04:30:30 +0400

    Better Humanode integration example (#72)
    * Use testnet 4
    
    * Proper Humanode-specific example
    
    * Another example
    
    * Switch to mainnet
    
    * Add newline at eof

--- Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
+++ Humanode/Humanode-starter/src/mappings/mappingHandlers.ts
@@ -1,49 +1,45 @@
-import {
-  SubstrateExtrinsic,
-  SubstrateEvent,
-  SubstrateBlock,
-} from "@subql/types";
-import { BioAuth, StarterEntity } from "../types";
-import { Balance } from "@polkadot/types/interfaces";
+import { Codec } from "@polkadot/types/types";
+import { Vec } from "@polkadot/types-codec";
+import { SubstrateEvent } from "@subql/types";
+import { BioauthNewAuthentication, ImOnlineSomeOffline } from "../types";
 
-const BIOAUTH_EXT_METHOD = Uint8Array.from(Buffer.from("0x0400", "hex"));
+export async function handleBioauthNewAuthenticationEvent(
+  substrateEvent: SubstrateEvent
+): Promise<void> {
+  const { event, block, idx } = substrateEvent;
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  //Create a new starterEntity with ID using block hash
-  let record = new StarterEntity(block.block.header.hash.toString());
-  //Record block number
-  record.field1 = block.block.header.number.toNumber();
-  await record.save();
-}
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
   const {
-    event: {
-      data: [account, balance],
-    },
+    data: [validatorPublicKey],
   } = event;
-  //Retrieve the record by its ID
-  const record = await StarterEntity.get(
-    event.block.block.header.hash.toString()
+
+  const record = new BioauthNewAuthentication(
+    `${block.block.header.number}-${idx}`
   );
-  record.field2 = account.toString();
-  //Big integer type Balance of a transfer event
-  record.field3 = (balance as Balance).toBigInt();
+  record.blockNumber = block.block.header.number.toNumber();
+  record.timestamp = block.timestamp;
+  record.validatorPublicKey = validatorPublicKey.toString();
   await record.save();
 }
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-  if (extrinsic.extrinsic.method.callIndex === BIOAUTH_EXT_METHOD) {
-    const record = new BioAuth(extrinsic.block.block.header.hash.toString());
-    //Record block number
-    record.blockNumber = extrinsic.block.block.header.number.toNumber();
-    record.signer = extrinsic.extrinsic.signer.toString();
-    record.signature = extrinsic.extrinsic.signature.toString();
+export async function handleImonlineSomeOfflineEvent(
+  substrateEvent: SubstrateEvent<[]>
+): Promise<void> {
+  const { event, block, idx } = substrateEvent;
 
-    record.ticket = extrinsic.extrinsic.method.args["req"]["ticket"];
-    record.ticketSignature =
-      extrinsic.extrinsic.method.args["req"]["ticketSignature"];
+  const {
+    data: [offline],
+  } = event;
 
-    await record.save();
+  const record = new ImOnlineSomeOffline(`${block.block.header.number}-${idx}`);
+  record.blockNumber = block.block.header.number.toNumber();
+  record.timestamp = block.timestamp;
+  record.accountIds = [];
+  for (const identification of offline as Vec<Codec>) {
+    const [accountId, _fullIdentification] = identification as any as [
+      Codec,
+      Codec
+    ];
+    record.accountIds.push(accountId.toString());
   }
+  await record.save();
 }
