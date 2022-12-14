e0cfa34935b5593211b4e8d6cdf66247a910c623
James Bayly <bayly.james@gmail.com>
2022-12-14 17:54:11 +1300

    Add Watr

---
+++ Watr/watr-starter/src/chaintypes.ts
@@ -0,0 +1,30 @@
+import type { OverrideBundleDefinition } from "@polkadot/types/types";
+
+/* eslint-disable sort-keys */
+/*
+// IT APPEARS THAT THERE ARE NO TYPE OVERRIDES FOR WATR
+
+
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        ParachainAccountIdOf: "AccountId",
+        Proof: {
+          leafHash: "Hash",
+          sortedHashes: "Vec<Hash>",
+        },
+        ProxyType: {
+          _enum: ["Any", "NonTransfer", "Governance", "_Staking", "NonProxy"],
+        },
+        RelayChainAccountId: "AccountId",
+        RootHashOf: "Hash",
+      },
+    },
+  ],
+};
+
+export default { typesBundle: { spec: { altair: definitions } } };
+*/
