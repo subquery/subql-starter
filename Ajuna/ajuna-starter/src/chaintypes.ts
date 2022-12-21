b0cd9653f685f8967840eda5a60ab11a3c3fd666
James Bayly <bayly.james@gmail.com>
2022-12-21 13:24:37 +1300

    Update Ajuna endpoint

--- Ajuna/ajuna-starter/src/chaintypes.ts
+++ Ajuna/ajuna-starter/src/chaintypes.ts
@@ -1,26 +1,17 @@
 import type { OverrideBundleDefinition } from "@polkadot/types/types";
 
 /* eslint-disable sort-keys */
-
 const definitions: OverrideBundleDefinition = {
   types: [
     {
       // on all versions
       minmax: [0, undefined],
       types: {
-        ParachainAccountIdOf: "AccountId",
-        Proof: {
-          leafHash: "Hash",
-          sortedHashes: "Vec<Hash>",
-        },
-        ProxyType: {
-          _enum: ["Any", "NonTransfer", "Governance", "_Staking", "NonProxy"],
-        },
-        RelayChainAccountId: "AccountId",
-        RootHashOf: "Hash",
+        Address: "MultiAddress",
+        LookupSource: "MultiAddress",
       },
     },
   ],
 };
 
-export default { typesBundle: { spec: { altair: definitions } } };
+export default { typesBundle: { spec: { ajuna: definitions } } };
