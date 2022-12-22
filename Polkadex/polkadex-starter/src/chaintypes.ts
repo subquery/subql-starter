f37a0c761879ed0e6c0825e98ccf6ea6e03dc059
seandotau <seandotau@gmail.com>
2022-12-22 14:09:32 +1100

    Adds Polkadex

---
+++ Polkadex/polkadex-starter/src/chaintypes.ts
@@ -0,0 +1,27 @@
+import type { OverrideBundleDefinition } from "@polkadot/types/types";
+
+/* eslint-disable sort-keys */
+
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        BurnTxDetails: {
+          approvals: 'u32',
+          approvers: 'Vec<AccountId>'
+        },
+        OrmlVestingSchedule: {
+          start: 'BlockNumber',
+          period: 'BlockNumber',
+          periodCount: 'u32',
+          perPeriod: 'Compact<Balance>'
+        },
+        VestingScheduleOf: 'OrmlVestingSchedule'
+      }
+    }
+  ]
+};
+
+export default { typesBundle: { spec: { polkadex: definitions } } };
