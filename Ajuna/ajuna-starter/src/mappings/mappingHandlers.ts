ca31e0cd0cc7e6e28b752bb9c825bfbccaaf337b
bz888 <ziyu.ben158@gmail.com>
2023-05-19 13:08:43 +1200

    updated ajuna

--- Ajuna/ajuna-starter/src/mappings/mappingHandlers.ts
+++ Ajuna/ajuna-starter/src/mappings/mappingHandlers.ts
@@ -8,9 +8,12 @@ import { Balance } from "@polkadot/types/interfaces";
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
   //Create a new starterEntity with ID using block hash
-  let record = new StarterEntity(block.block.header.hash.toString());
+  const record = StarterEntity.create({
+    id: block.block.header.hash.toString(),
+    field1: block.block.header.number.toNumber()
+  })
+
   //Record block number
-  record.field1 = block.block.header.number.toNumber();
   await record.save();
 }
 
@@ -24,19 +27,25 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
   const record = await StarterEntity.get(
     event.block.block.header.hash.toString()
   );
-  record.field2 = account.toString();
-  //Big integer type Balance of a transfer event
-  record.field3 = (balance as Balance).toBigInt();
-  await record.save();
+
+  if (record) {
+    record.field2 = account.toString();
+    //Big integer type Balance of a transfer event
+    record.field3 = (balance as Balance).toBigInt();
+    await record.save();
+  }
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
   const record = await StarterEntity.get(
     extrinsic.block.block.header.hash.toString()
   );
-  //Date type timestamp
-  record.field4 = extrinsic.block.timestamp;
-  //Boolean tyep
-  record.field5 = true;
-  await record.save();
+
+  if (record) {
+    //Date type timestamp
+    record.field4 = extrinsic.block.timestamp;
+    //Boolean tyep
+    record.field5 = true;
+    await record.save();
+  }
 }
