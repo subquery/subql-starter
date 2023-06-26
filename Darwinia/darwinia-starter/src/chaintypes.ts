7724e21ecb9a9293df355f2d703bcd9da86de0d4
fewensa <37804932+fewensa@users.noreply.github.com>
2023-06-26 13:49:36 +0800

    Update carwinia rpc and chain id

--- Darwinia/darwinia-starter/src/chaintypes.ts
+++
@@ -1,9 +0,0 @@
-import { typesBundleForPolkadot } from "@darwinia/types/mix";
-
-export default {
-  typesBundle: {
-    spec: {
-      darwinia: typesBundleForPolkadot.spec.darwinia,
-    },
-  },
-};
