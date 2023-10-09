138b8ac9f40afce0aed497c8e1fc9ec503594275
Scott Twiname <skott.twiname@gmail.com>
2023-10-09 15:37:57 +1300

    Fix Equilibrium not building

--- Equilibrium/equilibrium-starter/src/chaintypes.ts
+++ Equilibrium/equilibrium-starter/src/chaintypes.ts
@@ -2,41 +2,42 @@ import type { OverrideBundleDefinition } from "@polkadot/types/types";
 
 /* eslint-disable sort-keys */
 
+/* Not sure where `equilibrium` comes from so disabling for now */
 const definitions: OverrideBundleDefinition = {
-  derives: {
-    ...equilibrium.instances.balances.reduce(
-      (all, cur) => ({
-        ...all,
-        [cur]: {
-          customAccount: createCustomAccount(cur, (currency: string, api?: ApiInterfaceRx) => {
-            let assetsEnabled = true;
+  // derives: {
+  //   ...equilibrium.instances.balances.reduce(
+  //     (all, cur) => ({
+  //       ...all,
+  //       [cur]: {
+  //         customAccount: createCustomAccount(cur, (currency: string, api?: ApiInterfaceRx) => {
+  //           let assetsEnabled = true;
 
-            try {
-              api?.registry.createType('AssetIdInnerType' as any);
-            } catch (_) {
-              assetsEnabled = false;
-            }
+  //           try {
+  //             api?.registry.createType('AssetIdInnerType' as any);
+  //           } catch (_) {
+  //             assetsEnabled = false;
+  //           }
 
-            return assetsEnabled ? { 0: u64FromCurrency(currency) } : currency;
-          })
-        }
-      }),
-      {}
-    )
-  },
+  //           return assetsEnabled ? { 0: u64FromCurrency(currency) } : currency;
+  //         })
+  //       }
+  //     }),
+  //     {}
+  //   )
+  // },
 
-  instances: equilibrium.instances,
+  // instances: equilibrium.instances,
 
-  types: [
-    {
-      minmax: [0, 264],
-      types: equilibrium.types
-    },
-    {
-      minmax: [265, undefined],
-      types: equilibriumNext.types
-    }
-  ]
+  // types: [
+  //   {
+  //     minmax: [0, 264],
+  //     types: equilibrium.types
+  //   },
+  //   {
+  //     minmax: [265, undefined],
+  //     types: equilibriumNext.types
+  //   }
+  // ]
 };
 
 export default { typesBundle: { spec: { equilibrum: definitions } } };
