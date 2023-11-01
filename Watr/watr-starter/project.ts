5eb670eb1824a79adb9aa0aee39ace37b4ab1da5
James Bayly <bayly.james@gmail.com>
2023-11-01 13:16:42 +0800

    Update Watr chainid

--- Watr/watr-starter/project.ts
+++ Watr/watr-starter/project.ts
@@ -27,7 +27,7 @@ const project: SubstrateProject = {
   network: {
     /* The genesis hash of the network (hash of block 0) */
     chainId:
-      "0xb53c620c41860278fa3068a5367c8eedceefce8a7c29237d830bc09a71737b5d",
+      "0x161db6cdc5896fe55ef12b4778fe78dd65d7af43f65c601786b88d7a93ebc58a",
     /**
      * This endpoint must be a public non-pruned archive node
      * Public nodes may be rate limited, which can affect indexing speed
