6be07937dadbb79267fb0791fcc717f559b9d723
bgodlin <37313677+bgodlin@users.noreply.github.com>
2024-01-18 02:59:00 +0100

    Featured Manta (#103)

---
+++ Manta/manta-atlantic-starter/project.ts
@@ -0,0 +1,80 @@
+import {
+  SubstrateDatasourceKind,
+  SubstrateHandlerKind,
+  SubstrateProject,
+} from "@subql/types";
+
+// Can expand the Datasource processor types via the genreic param
+const project: SubstrateProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "manta-atlantic-starter",
+  description:
+    "This project can be used as a starting point for developing your SubQuery project. It indexes all transfers on Manta Atlantic network",
+  runner: {
+    node: {
+      name: "@subql/node",
+      version: ">=3.0.1",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /* The genesis hash of the network (hash of block 0) */
+    chainId:
+      "0xf3c7ad88f6a80f366c4be216691411ef0622e8b809b1046ea297ef106058d4eb",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["wss://ws.manta.systems"],
+    chaintypes: {
+      file: "./dist/chaintypes.js",
+    },
+  },
+  dataSources: [
+    {
+      kind: SubstrateDatasourceKind.Runtime,
+      startBlock: 1,
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          /*{
+            kind: SubstrateHandlerKind.Block,
+            handler: "handleBlock",
+            filter: {
+              modulo: 100,
+            },
+          },*/
+          /*{
+            kind: SubstrateHandlerKind.Call,
+            handler: "handleCall",
+            filter: {
+              module: "balances",
+            },
+          },*/
+          {
+            kind: SubstrateHandlerKind.Event,
+            handler: "handleEvent",
+            filter: {
+              module: "balances",
+              method: "Transfer",
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+// Must set default to the project instance
+export default project;
