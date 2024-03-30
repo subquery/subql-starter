32b99865905554b302f34d47466fac12eaec1280
godlin <bogodline@gmail.com>
2024-03-30 19:51:45 +0100

    Featured Bajun

---
+++ Bajun/bajun-starter/src/chaintypes.ts
@@ -0,0 +1,17 @@
+import type { OverrideBundleDefinition } from "@polkadot/types/types";
+
+/* eslint-disable sort-keys */
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        Address: "MultiAddress",
+        LookupSource: "MultiAddress",
+      },
+    },
+  ],
+};
+
+export default definitions;
