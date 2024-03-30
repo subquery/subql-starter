e274b73036f9be2e67ea0c0d9730b1504a67dee8
godlin <bogodline@gmail.com>
2024-03-30 20:15:22 +0100

    Featured Avail

---
+++ Avail/avail-starter/project.ts
@@ -0,0 +1,67 @@
+import {
+  SubstrateDatasourceKind,
+  SubstrateHandlerKind,
+  SubstrateProject,
+} from "@subql/types";
+
+// Can expand the Datasource processor types via the genreic param
+const project: SubstrateProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "avail-starter",
+  description:
+    "This project can be used as a starting point for developing your SubQuery project. It indexes all transfers on Avail network",
+  runner: {
+    node: {
+      name: "@subql/node",
+      version: ">=3.0.1",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /* The genesis hash of the network (hash of block 0) */
+    chainId:
+      "0x6f09966420b2608d1947ccfb0f2a362450d1fc7fd902c29b67c906eaa965a7ae",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["wss://rpc-testnet.avail.tools/ws"],
+    chaintypes: {
+      file: "./types.json",
+    },
+    bypassBlocks: ["272000-280500"],
+  },
+  dataSources: [
+    {
+      kind: SubstrateDatasourceKind.Runtime,
+      startBlock: 1,
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: SubstrateHandlerKind.Event,
+            handler: "handleEvent",
+            filter: {
+              module: "balances",
+              method: "Transfer",
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+// Must set default to the project instance
+export default project;
